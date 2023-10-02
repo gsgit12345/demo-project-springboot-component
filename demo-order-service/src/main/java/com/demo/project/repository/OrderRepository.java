5073f02b08f420bbcd7aa735951249a0327f64bc
ghanshyam shukla <gsgmca682@gmail.com>
2023-10-02 09:06:55 -0700

    this is my commit message

--- demo-order-service/src/main/java/com/demo/project/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.project.repository;
-
-import com.demo.project.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
